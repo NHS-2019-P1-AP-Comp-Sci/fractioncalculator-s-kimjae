5eef4053d87e96ad3f7a4d8827cdf2f2b7c23a3b
s-kimjae <s-kimjae@53S500036103.it.bsd405.org>
2019-11-15 08:46:01 -0800

    Turning in checkpoint 1

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -3,11 +3,16 @@
  */
 
 package fracCalc;
-
+import java.util.*; 
 public class FracCalc {
 
     public static void main(String[] args)
     {
+    	System.out.println("Welcome to the Frac Calc Calculator");
+    	Scanner userInput = new Scanner(System.in);
+    	System.out.print("Enter an expression");
+    	String expression = userInput.nextLine();
+    	System.out.println(produceAnswer(expression));
         // TODO: Read the input from the user and call produceAnswer with an equation
 
     }
@@ -20,13 +25,26 @@ public class FracCalc {
     //
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
-        // TODO: Implement this function to produce the solution to the input
+    public static String produceAnswer(String expression) {
+        String fraction = "";
+        String operator = "";
+        String fraction2 = "";
+        int i = 0;
+        while (fraction.equals("")) {
+        	if (expression.charAt(i) == (' ')) {
+        		fraction = expression.substring(0, i);
+        		operator = expression.substring(i + 1, i + 2);
+        		fraction2 = expression.substring(i + 3);
+        			
+      		} else {
+       			i++;
+       		}
+    	// TODO: Implement this function to produce the solution to the input
 
-        return "";
-    }
 
+        }
+        return fraction2;
     // TODO: Fill in the space below with any helper methods that you think you will need
 
+    }
 }
